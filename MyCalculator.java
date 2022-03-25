77fe64e2e51fde30729e22fbcd367e6b4f2538b9
vinnurs <74790287+vinnurs@users.noreply.github.com>
2022-03-25 18:53:21 +0530

    Update MyCalculator.java

--- MyCalculator.java
+++ MyCalculator.java
@@ -3,10 +3,10 @@ package week2.day2;
 public class MyCalculator {
 	public static void main(String[] args) {
 	Calculator calc= new Calculator();
-	System.out.println("Addition:" + calc.add(9, 3, 5));
-	System.out.println("Subtraction:" + calc.sub(90, 5));
-	System.out.println("Multiplication:" + calc.mul(10000, 5));
-	System.out.println("Division:" + calc.divide(10.5f, 7f));
+	System.out.println("Addition:" + calc.add(27, 3, 5));
+	System.out.println("Subtraction:" + calc.sub(120, 5));
+	System.out.println("Multiplication:" + calc.mul(1000, 5));
+	System.out.println("Division:" + calc.divide(12.3f, 7f));
 
 		}
 	}
